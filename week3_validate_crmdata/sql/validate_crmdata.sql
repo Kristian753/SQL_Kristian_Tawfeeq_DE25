fb06f32ec8b89e85d582a746ed0e51c9f87e5796
Kristian Tawfeeq <kristiantawfeeq@Mac.lan>
2025-12-09 13:05:18 +0100

    week4

--- week3_validate_crmdata/sql/validate_crmdata.sql
+++ week3_validate_crmdata/sql/validate_crmdata.sql
@@ -7,3 +7,20 @@ CREATE TABLE IF NOT EXISTS staging.crmdata_new AS (
 CREATE TABLE IF NOT EXISTS staging.crmdata_old AS (
     SELECT * FROM read_csv_auto('data/crm_old.csv')
 );
+
+
+
+
+
+/* Task 2 */
+-- find invalid emails
+-- use LIKE operator with wildcards for the old data
+
+SELECT * FROM staging.crmdata_old
+WHERE NOT email LIKE '%@%.%';
+
+-- use REGEXP functions for the new data
+-- because the above query cannot 
+
+SELECT * FROM staging.crmdata_new
+WHERE regexp_matches(email, '[A-Za-z0-9]+@[A-Za-z]+\.[A-Za-z]+');
